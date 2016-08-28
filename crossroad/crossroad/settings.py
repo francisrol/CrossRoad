91b37cffbb76ab5df6e31997dce3c0ff1aaa275a
francisrol <610960903@qq.com>
2016-08-28 16:48:54 +0800

    Update settings.py

--- crossroad/crossroad/settings.py
+++ crossroad/crossroad/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0)^w%$8!j75kalb40zu)k!3$c1t66cekupkjtq_t-jtl(qdo6b'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
